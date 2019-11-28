5f2d3526a870cfa7a43e39d44ff90b8e21f39f6a
marven <2365702941@qq.com>
2019-11-28 17:01:12 +0800

    项目结构创建完成

---
+++ gmall-admin-web/src/test/java/com/ppt/gmall/admin/GmallAdminWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ppt.gmall.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallAdminWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
